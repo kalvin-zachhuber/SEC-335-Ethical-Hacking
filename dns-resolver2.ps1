7a68ca2f63aa82511190bae1e81a91424c4faa1f
kalvin-zachhuber <113249152+kalvin-zachhuber@users.noreply.github.com>
2024-09-26 11:26:07 -0400

    Add files via upload

---
+++ dns-resolver2.ps1
@@ -0,0 +1,21 @@
+﻿param (
+    [string]$NetworkPrefix,
+    [string]$DnsServer
+)
+#looping through the /24 hosts
+For ($i = 1; $i -le 254; $i++) {
+    $ipAddress = "$NetworkPrefix.$i"
+
+    # Performing the DNS lookup
+    try {
+        $dnsResult = Resolve-DnsName -Server $DnsServer -Name $ipAddress -ErrorAction Stop
+        $hostname = $dnsResult.NameHost
+
+        # Output the succesfull IP's and their hostname
+        Write-Host "$ipAddress $hostname"
+    } catch {
+        continue
+    }
+}
+        
+    
